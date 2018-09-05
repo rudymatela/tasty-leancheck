a3e21da9de413dc4d77ea69f12c51ed523349276
Rudy Matela <rudy@matela.com.br>
2018-09-05 19:06:03 -0300

    Improve tests.

--- tests/test.hs
+++ tests/test.hs
@@ -10,16 +10,30 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [properties]
+-- tests = testGroup "Tests (checked by LeanCheck)" [passing]
+tests = testGroup "Tests (checked by LeanCheck)" [passing,failing,erroring]
 
--- loosely based on the official Tasty example
-properties :: TestTree
-properties = testGroup "(checked by LeanCheck)"
+-- tests that pass
+passing :: TestTree
+passing = testGroup "Passing tests"
   [ LC.testProperty "sort . sort == sort" $
       \xs -> sort (sort xs :: [Int]) == sort xs
   , LC.testProperty "Fermat's little theorem" $
       \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
--- The following fails (and should):
---, LC.testProperty "Fermat's last theorem" $
---    \x y z n -> (n :: Integer) >= 3 ==> x^n + y^n /= (z^n :: Integer)
+  , LC.testProperty "divMod" $
+      \x y -> y /= 0 ==> y * (x `div` y) + x `mod` y == (x :: Int)
+  ]
+
+-- tests that fail (and should)
+failing :: TestTree
+failing = testGroup "Failing tests"
+  [ LC.testProperty "Fermat's last theorem" $
+      \x y z n -> (n :: Integer) >= 3 ==> x^n + y^n /= (z^n :: Integer)
+  ]
+
+-- tests that err (and should)
+erroring :: TestTree
+erroring = testGroup "Erroring tests"
+  [ LC.testProperty "divMod (unguarded)" $
+      \x y -> y * (x `div` y) + x `mod` y == (x :: Int)
   ]
