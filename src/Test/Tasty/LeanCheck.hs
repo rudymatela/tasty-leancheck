b2356e0850c29103af4faaf02f3a31802ddf1db7
Rudy Matela <rudy@matela.com.br>
2018-09-04 21:33:59 -0300

    some boilerplate...

--- src/Test/Tasty/LeanCheck.hs
+++ src/Test/Tasty/LeanCheck.hs
@@ -8,13 +8,24 @@ where
 import Test.Tasty.Providers
 import Test.Tasty.Options
 import Test.LeanCheck
+import Data.Proxy
 
-testProperty :: a
-testProperty = undefined
+-- | Create a 'Test' for a LeanCheck 'Testable' property
+testProperty :: Testable a => TestName -> a -> TestTree
+testProperty name prop = singleTest name $ holds 100 prop
+-- TODO: change to the configured value
 
 newtype LeanCheckTests = LeanCheckTests Int
   deriving (Show, Eq, Ord)
 
 -- TODO: instance IsOption LeanCheckTests
 -- TODO: instance IsTest Bool ??
+instance IsOption LeanCheckTests where
+  defaultValue = LeanCheckTests 200
+  parseValue = fmap LeanCheckTests . safeRead
+  optionName = return "leancheck-depth"
+  optionHelp = return "Depth to use for leancheck tests"
 
+instance IsTest Bool where
+  testOptions = return [Option (Proxy :: Proxy LeanCheckTests)]
+  run opts prop callback = undefined
