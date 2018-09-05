7d9f838f702ec8c36a24b255af7933b9e5a75dfa
Rudy Matela <rudy@matela.com.br>
2018-09-05 17:51:15 -0300

    Add a couple tests.

--- tests/test.hs
+++ tests/test.hs
@@ -2,8 +2,24 @@
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 {-# LANGUAGE CPP #-}
 import Test.Tasty
-import Test.Tasty.LeanCheck
+import Test.Tasty.LeanCheck as LC
+
+import Data.List (sort)
 
 main :: IO ()
-main = do
-  putStrLn "TODO: write me"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties]
+
+-- loosely based on the official Tasty example
+properties :: TestTree
+properties = testGroup "(checked by LeanCheck)"
+  [ LC.testProperty "sort . sort == sort" $
+      \xs -> sort (sort xs :: [Int]) == sort xs
+  , LC.testProperty "Fermat's little theorem" $
+      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+-- The following fails (and should):
+--, LC.testProperty "Fermat's last theorem" $
+--    \x y z n -> (n :: Integer) >= 3 ==> x^n + y^n /= (z^n :: Integer)
+  ]
