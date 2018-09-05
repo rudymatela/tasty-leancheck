c55adcad5cb3af0f521df0fc24687b313fc567b1
Rudy Matela <rudy@matela.com.br>
2018-09-05 19:07:05 -0300

    T.T.LeanCheck: catch Exceptions, improve output

--- src/Test/Tasty/LeanCheck.hs
+++ src/Test/Tasty/LeanCheck.hs
@@ -14,23 +14,22 @@ module Test.Tasty.LeanCheck
   )
 where
 
-import Test.Tasty.Providers
+import Test.Tasty.Providers hiding (Result)
 import Test.Tasty.Options
 import Test.LeanCheck
 import Data.Proxy
+import Control.Exception (SomeException, catch, evaluate)
 
-data TestResult = Ok
-                | Failed String
+newtype Results = Results [([String],Bool)]
 
-toTestResult :: Maybe [String] -> TestResult
-toTestResult Nothing = Ok
-toTestResult (Just ce) = Failed $ unwords ce
+data Result = OK        Int
+            | Falsified Int [String]
+            | Exception Int [String] String
+  deriving (Eq, Show)
 
 -- | Create a 'Test' for a LeanCheck 'Testable' property.
 testProperty :: Testable a => TestName -> a -> TestTree
-testProperty name prop = singleTest name $ toTestResult $ counterExample 200 prop
--- TODO: change to the configured value
---       to do this, maybe I'll have to wrap resultiers in the TestResult
+testProperty name = singleTest name . Results . results
 
 -- | Number of test cases for LeanCheck to generate.
 newtype LeanCheckTests = LeanCheckTests Int
@@ -42,7 +41,47 @@ instance IsOption LeanCheckTests where
   optionName = return "leancheck-tests"
   optionHelp = return "Depth to use for leancheck tests"
 
-instance IsTest TestResult where
+instance IsTest Results where
   testOptions = return [Option (Proxy :: Proxy LeanCheckTests)]
-  run _ Ok          _ = pure $ testPassed ""
-  run _ (Failed ce) _ = pure $ testFailed $ unlines ["*** Failure:", ce, ""]
+  run _ results _ = do
+    r <- resultIO m results
+    pure $ case r of
+      OK n             -> testPassed $ "+++ OK, passed " ++ show n ++ " tests"
+                                    ++ takeWhile (\_ -> n < m) " (exhausted)"
+                                    ++ "."
+      Falsified i ce   -> testFailed $ "*** Failed! Falsifiable (after "
+                                    ++ show i ++ " tests):\n" ++ joinArgs ce
+      Exception i ce e -> testFailed $ "*** Failed! Exception '" ++ e
+                                    ++ "' (after " ++ show i ++ " tests):\n"
+                                    ++ joinArgs ce
+    where
+    m = 200
+
+resultsIO :: Int -> Results -> [IO Result]
+resultsIO n (Results results) = zipWith torio [1..] $ take n results
+  where
+    tor i (_,True) = OK i
+    tor i (as,False) = Falsified i as
+    torio i r@(as,_) = evaluate (tor i r)
+       `catch` \e -> let _ = e :: SomeException
+                     in return (Exception i as (show e))
+
+resultIO :: Int -> Results -> IO Result
+resultIO n = computeResult . resultsIO n
+  where
+  computeResult []  = error "resultIO: no results, empty Listable enumeration?"
+  computeResult [r] = r
+  computeResult (r:rs) = r >>= \r -> case r of
+                                     (OK _) -> computeResult rs
+                                     _      -> return r
+
+-- joins the counter-example arguments
+joinArgs :: [String] -> String
+joinArgs ce | any ('\n' `elem`) ce = unlines $ map chopBreak ce
+            | otherwise            = unwords ce
+
+-- chops a line break at the end if there is any
+chopBreak :: String -> String
+chopBreak [] = []
+chopBreak ['\n'] = []
+chopBreak (x:xs) = x:chopBreak xs
