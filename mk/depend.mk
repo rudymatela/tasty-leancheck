9b54b792cb09f5372270c6af5b7b652fe3178ebb
Rudy Matela <rudy@matela.com.br>
2020-03-28 20:43:06 -0300

    rename tests/ to test/

--- mk/depend.mk
+++ mk/depend.mk
@@ -1,19 +1,22 @@
+eg/minimal: \
+  eg/minimal.hs \
+  mk/toplibs
+eg/minimal.o: \
+  src/Test/Tasty/LeanCheck.hs \
+  eg/minimal.hs
 mk/All.o: \
   src/Test/Tasty/LeanCheck.hs \
   mk/All.hs
 mk/Toplibs.o: \
   src/Test/Tasty/LeanCheck.hs \
   mk/Toplibs.hs
-Setup.o: \
-  Setup.hs
-Setup: \
-  Setup.hs \
+src/Test/Tasty/LeanCheck: \
   mk/toplibs
 src/Test/Tasty/LeanCheck.o: \
   src/Test/Tasty/LeanCheck.hs
-tests/test.o: \
-  tests/test.hs \
+test/test.o: \
+  test/test.hs \
   src/Test/Tasty/LeanCheck.hs
-tests/test: \
-  tests/test.hs \
+test/test: \
+  test/test.hs \
   mk/toplibs
